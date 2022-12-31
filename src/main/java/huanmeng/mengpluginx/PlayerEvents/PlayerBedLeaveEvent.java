a55d06f71660e9ef4d0af2685a6e55a4b595354a
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2022-12-31 13:59:23 +0800

    Create PlayerBedLeaveEvent.java

---
+++ src/main/java/huanmeng/mengpluginx/PlayerEvents/PlayerBedLeaveEvent.java
@@ -0,0 +1,29 @@
+package huanmeng.mengpluginx.PlayerEvents;
+
+import huanmeng.mengpluginx.MengPluginX;
+
+import org.bukkit.ChatColor;
+
+import org.bukkit.entity.Player;
+
+import org.bukkit.event.EventHandler;
+
+import org.bukkit.event.Listener;
+
+import org.bukkit.plugin.Plugin;
+
+public class PlayerBedLeaveEvent implements Listener {
+
+    Plugin plugin = huanmeng.mengpluginx.MengPluginX.getPlugin(huanmeng.mengpluginx.MengPluginX.class);
+
+    @EventHandler
+
+    public void PlayerBedLeaveEvent(org.bukkit.event.player.PlayerBedLeaveEvent BedLeave){
+
+        Player getup = BedLeave.getPlayer();
+
+        getup.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Getup"));
+
+    }
+
+}
