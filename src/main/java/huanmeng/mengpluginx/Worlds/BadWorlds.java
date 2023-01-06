5ffda27203f3d23503917922282b7797131f106d
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-01-06 19:08:37 +0800

    Add files via upload

--- src/main/java/huanmeng/mengpluginx/Worlds/BadWorlds.java
+++ src/main/java/huanmeng/mengpluginx/Worlds/BadWorlds.java
@@ -1,46 +1,25 @@
 package huanmeng.mengpluginx.Worlds;
 
 import huanmeng.mengpluginx.MengPluginX;
-
 import org.bukkit.ChatColor;
-
 import org.bukkit.entity.Player;
-
 import org.bukkit.event.EventHandler;
-
 import org.bukkit.event.Listener;
-
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
 import org.bukkit.plugin.Plugin;
 
-import java.util.Objects;
 
 public class BadWorlds implements Listener {
-
     Plugin plugin = MengPluginX.getPlugin(MengPluginX.class);
-
     @EventHandler
-
     public void PlayerChat(AsyncPlayerChatEvent event) {
-
         Player player = (Player) event.getPlayer();
-
         String massage = event.getMessage();
-
         for (int i = 0; i < plugin.getConfig().getStringList("BadWorlds").size(); i++) {
-
             if (massage.contains(plugin.getConfig().getStringList("BadWorlds").get(i))) {
-
                 event.setCancelled(plugin.getConfig().getBoolean("enable"));
-
                 player.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
             }
-
         }
-
     }
-
 }
-
