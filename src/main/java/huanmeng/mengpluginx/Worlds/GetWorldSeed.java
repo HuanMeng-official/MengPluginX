db205f2c23cddce670c4b29867930926660b7626
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-01-14 17:58:23 +0800

    Create GetWorldSeed.java

---
+++ src/main/java/huanmeng/mengpluginx/Worlds/GetWorldSeed.java
@@ -0,0 +1,39 @@
+package huanmeng.mengpluginx.Worlds;
+
+import org.bukkit.ChatColor;
+
+import org.bukkit.command.Command;
+
+import org.bukkit.command.CommandExecutor;
+
+import org.bukkit.command.CommandSender;
+
+import org.bukkit.entity.Player;
+
+import org.jetbrains.annotations.NotNull;
+
+public class GetWorldSeed implements CommandExecutor {
+
+    @Override
+
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (sender instanceof Player){
+
+            Player player = (Player) sender;
+
+            String seed = String.valueOf(player.getWorld().getSeed());
+
+            sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "种子：" + ChatColor.GREEN + seed);
+
+        }else {
+
+            System.out.println("Error: Please let the player execute the command");
+
+        }
+
+        return false;
+
+    }
+
+}
