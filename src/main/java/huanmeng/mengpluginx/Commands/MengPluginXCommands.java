86230a7b9b5e991629b636867793916a00bc7de3
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-01-15 20:38:22 +0800

    Add files via upload

--- src/main/java/huanmeng/mengpluginx/Commands/MengPluginXCommands.java
+++ src/main/java/huanmeng/mengpluginx/Commands/MengPluginXCommands.java
@@ -9,12 +9,10 @@ import org.bukkit.entity.Player;
 public class MengPluginXCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player){
+        if (args.length == 0){
             Player player = (Player) sender;
             sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "插件已启用了哦(ᗒᗩᗕ)՞");
             sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "如果Ping和TPS命令无法使用请输入papi ecloud download Player和papi ecloud download Server命令下载PAPI依赖并重启PAPI");
-        }else{
-            System.out.println("Error: Please let the player execute the command");
         }
         return false;
     }
