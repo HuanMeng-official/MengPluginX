cf49c539d4ea2783287c2eb4d6c7dd8a4c846e93
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-01-18 23:53:27 +0800

    Add files via upload

--- src/main/java/huanmeng/mengpluginx/MengPluginX.java
+++ src/main/java/huanmeng/mengpluginx/MengPluginX.java
@@ -5,8 +5,8 @@ import huanmeng.mengpluginx.PlayerEvents.*;
 import huanmeng.mengpluginx.PlaceholderAPI.*;
 import huanmeng.mengpluginx.Words.*;
 import huanmeng.mengpluginx.Worlds.*;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
@@ -23,10 +23,7 @@ public final class MengPluginX extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerBedLeaveEvent(),this);
         getServer().getPluginManager().registerEvents(new BadWords(),this);
         Objects.requireNonNull(getCommand("mpx")).setExecutor(new MengPluginXCommands());
-        Objects.requireNonNull(getCommand("gm0")).setExecutor(new SetSurvival());
-        Objects.requireNonNull(getCommand("gm1")).setExecutor(new SetCreative());
-        Objects.requireNonNull(getCommand("gm2")).setExecutor(new SetAdventure());
-        Objects.requireNonNull(getCommand("gm3")).setExecutor(new SetSpectator());
+        Objects.requireNonNull(getCommand("gm")).setExecutor(new SetGameMode());
         Objects.requireNonNull(getCommand("mpx_tps")).setExecutor(new GetServerTPS());
         Objects.requireNonNull(getCommand("mpx_ping")).setExecutor(new GetPlayerPing());
         Objects.requireNonNull(getCommand("online_player")).setExecutor(new GetOnlinePlayer());
@@ -38,6 +35,6 @@ public final class MengPluginX extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println(ChatColor.GREEN + "插件正在关闭");
+        System.out.println("插件正在关闭");
     }
 }
