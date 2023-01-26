30dac3c8a2a10eadb197137f9bcb8a79d31b3111
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-01-26 23:59:50 +0800

    fix some bug

--- en-us/src/main/java/huanmeng/mengpluginx/Commands/MengPluginXCommands.java
+++ en-us/src/main/java/huanmeng/mengpluginx/Commands/MengPluginXCommands.java
@@ -12,11 +12,11 @@ public class MengPluginXCommands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player player = (Player) sender;
-            sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "Plugin is running(ᗒᗩᗕ)՞");
-            sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "If the ping and TPS commands do not work, enter the papi ecloud download Player and papi ecloud download Server commands to download the PAPI dependencies and restart PAPI");
+            sender.sendMessage(ChatColor.YELLOW + "[HuanMeng]" + ChatColor.WHITE + "Plugin is running(ᗒᗩᗕ)՞");
+            sender.sendMessage(ChatColor.YELLOW + "[HuanMeng]" + ChatColor.WHITE + "If the ping and TPS commands do not work, enter the papi ecloud download Player and papi ecloud download Server commands to download the PAPI dependencies and restart PAPI");
         } else if (sender instanceof ConsoleCommandSender) {
-            System.out.println(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "Plugin is running");
-            System.out.println(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "If the ping and TPS commands do not work, enter the papi ecloud download Player and papi ecloud download Server commands to download the PAPI dependencies and restart PAPI");
+            System.out.println(ChatColor.YELLOW + "[HuanMeng]" + ChatColor.WHITE + "Plugin is running");
+            System.out.println(ChatColor.YELLOW + "[HuanMeng]" + ChatColor.WHITE + "If the ping and TPS commands do not work, enter the papi ecloud download Player and papi ecloud download Server commands to download the PAPI dependencies and restart PAPI");
         }
         return false;
     }
