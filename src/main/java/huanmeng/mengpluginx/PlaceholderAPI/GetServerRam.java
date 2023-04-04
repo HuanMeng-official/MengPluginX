c09f13420acddf8feb00d654158b6815a7a1fbc6
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-04-04 19:49:28 +0800

    添加两条子命令

--- src/main/java/huanmeng/mengpluginx/PlaceholderAPI/GetServerRam.java
+++ src/main/java/huanmeng/mengpluginx/PlaceholderAPI/GetServerRam.java
@@ -2,13 +2,9 @@ package huanmeng.mengpluginx.PlaceholderAPI;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +14,23 @@ public class GetServerRam implements CommandExecutor , TabExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender.hasPermission("mpx.huanmeng.op")) {
             Player player = (Player) sender;
-            //已使用RAM
             String used_ram = "%server_ram_used%";
             used_ram = PlaceholderAPI.setPlaceholders(player,used_ram);
-            //未使用RAM
             String free_ram = "%server_ram_free%";
             free_ram = PlaceholderAPI.setPlaceholders(player,free_ram);
+            String total_ram = "%server_ram_total%";
+            total_ram = PlaceholderAPI.setPlaceholders(player,total_ram);
             String ram = args[0];
             switch (ram) {
                 case "used":
-                    player.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "当前服务器已用内存：" + ChatColor.GREEN + used_ram);
+                    player.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "当前服务器已用内存：" + ChatColor.GREEN + used_ram + ChatColor.WHITE + "MB");
                     break;
                 case "free":
-                    player.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "当前服务器剩余内存：" + ChatColor.GREEN + free_ram);
+                    player.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "当前服务器剩余内存：" + ChatColor.GREEN + free_ram + ChatColor.WHITE + "MB");
+                    break;
+                case "total":
+                    player.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.WHITE + "当前服务器总共内存：" + ChatColor.GREEN + total_ram + ChatColor.WHITE + "MB");
+                    break;
             }
         }else {
             System.out.println(ChatColor.RED + "Error: Please let the player execute the command");
@@ -44,6 +44,7 @@ public class GetServerRam implements CommandExecutor , TabExecutor {
             List<String> list = new ArrayList<>();
             list.add("used");
             list.add("free");
+            list.add("total");
             return list;
         }
         return null;
