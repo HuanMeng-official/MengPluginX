6e52f0e5e326d7a73db7a7d7cd01a54d0d397b62
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-04-05 14:20:53 +0800

    添加TickPerSecond.java

--- src/main/java/huanmeng/mengpluginx/MengPluginX.java
+++ src/main/java/huanmeng/mengpluginx/MengPluginX.java
@@ -3,9 +3,9 @@ package huanmeng.mengpluginx;
 import huanmeng.mengpluginx.Commands.*;
 import huanmeng.mengpluginx.PlayerEvents.*;
 import huanmeng.mengpluginx.PlaceholderAPI.*;
+import huanmeng.mengpluginx.Tools.*;
 import huanmeng.mengpluginx.Words.*;
 import huanmeng.mengpluginx.Worlds.*;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -14,18 +14,13 @@ public final class MengPluginX extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        System.out.println(ChatColor.GREEN + "===================");
         System.out.println("插件正在加载");
-        System.out.println("正在加载config.yml");
-        System.out.println("正在加载keywords.yml");
-        System.out.println("插件版本：4.0-r1.1");
-        System.out.println("插件作者：幻梦official");
-        System.out.println(ChatColor.GREEN + "===================");
     }
 
     @Override
     public void onEnable() {
         System.out.println("插件成功加载");
+        getServer().getScheduler().scheduleSyncDelayedTask(this,new TickPerSecond(),100L);
         saveResource("keywords.yml",false);
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
@@ -36,8 +31,7 @@ public final class MengPluginX extends JavaPlugin {
         Objects.requireNonNull(getCommand("mpx")).setExecutor(new MengPluginXCommands());
         Objects.requireNonNull(getCommand("mpx_help")).setExecutor(new CommandHelp());
         Objects.requireNonNull(getCommand("gm")).setExecutor(new SetGameMode());
-        Objects.requireNonNull(getCommand("gm")).setTabCompleter(new SetGameMode());
-        Objects.requireNonNull(getCommand("mpx_tps")).setExecutor(new GetServerTPS());
+        Objects.requireNonNull(getCommand("mpx_tps")).setExecutor(new GetTickPerSecond());
         Objects.requireNonNull(getCommand("mpx_ping")).setExecutor(new GetPlayerPing());
         Objects.requireNonNull(getCommand("online_player")).setExecutor(new GetOnlinePlayer());
         Objects.requireNonNull(getCommand("mpx_seed")).setExecutor(new GetWorldSeed());
@@ -50,6 +44,6 @@ public final class MengPluginX extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        System.out.println("插件正在关闭");
+        System.out.println("插件成功关闭");
     }
 }
