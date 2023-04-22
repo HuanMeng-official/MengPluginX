499e5582891e509fdb5b7796964db98d3f52c46a
HuanMeng-official <3099044113@qq.com>
2023-04-22 22:33:29 +0800

    delted antiCreeperBoom

--- src/main/java/top/huanmeng/MengPluginX.java
+++ src/main/java/top/huanmeng/MengPluginX.java
@@ -17,16 +17,42 @@ public final class MengPluginX extends JavaPlugin {
 
     @Override
     public void onLoad() {
+        System.out.println(ChatColor.DARK_RED + "MengPluginX is loading");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println(ChatColor.DARK_GREEN + "=========================");
         System.out.println(ChatColor.GOLD + "__  __ ______  __");
         System.out.println(ChatColor.GOLD + "|  \\/  |  _ \\ \\/ /");
         System.out.println(ChatColor.GOLD + "| |\\/| | |_)  \\  /");
         System.out.println(ChatColor.GOLD + "| |  | |  __/ /  \\");
         System.out.println(ChatColor.GOLD + "|_|  |_|_|  /_/\\_\\");
-    }
-
-    @Override
-    public void onEnable() {
-        System.out.println("插件成功加载");
+        int pluginId = 18219;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
+        getServer().getScheduler().scheduleSyncDelayedTask(this,new TickPerSecond(),100L);
+        saveResource("keywords.yml",false);
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new PlayerJoinEvent(),this);
+        getServer().getPluginManager().registerEvents(new PlayerBedEnterEvent(),this);
+        getServer().getPluginManager().registerEvents(new PlayerBedLeaveEvent(),this);
+        getServer().getPluginManager().registerEvents(new BadWords(),this);
+        getServer().getPluginManager().registerEvents(new KeepInventory(),this);
+        getServer().getPluginManager().registerEvents(new AntiCreeper(),this);
+        getServer().getPluginManager().registerEvents(new AntiEnderMan(),this);
+        Objects.requireNonNull(getCommand("mpx")).setExecutor(new MengPluginXInfo());
+        Objects.requireNonNull(getCommand("gm")).setExecutor(new SetGameMode());
+        Objects.requireNonNull(getCommand("mpx_tps")).setExecutor(new GetTickPerSecond());
+        Objects.requireNonNull(getCommand("mpx_ping")).setExecutor(new GetPlayerPing());
+        Objects.requireNonNull(getCommand("online_player")).setExecutor(new GetOnlinePlayer());
+        Objects.requireNonNull(getCommand("mpx_seed")).setExecutor(new GetWorldSeed());
+        Objects.requireNonNull(getCommand("mpx_time")).setExecutor(new GetFullTime());
+        Objects.requireNonNull(getCommand("mpx_uuid")).setExecutor(new GetPlayerUUID());
+        Objects.requireNonNull(getCommand("mpx_ip")).setExecutor(new GetPlayerIP());
+        Objects.requireNonNull(getCommand("mpx_sa")).setExecutor(new SendAnnouncement());
+        Objects.requireNonNull(getCommand("mpx_ram")).setExecutor(new GetServerRam());
         boolean keepInventory = getConfig().getBoolean("KeepInventory", true);
         boolean antiCreeper = getConfig().getBoolean("AntiCreeper",true);
         boolean antiEnderMan = getConfig().getBoolean("AntiEnderMan",true);
@@ -45,36 +71,11 @@ public final class MengPluginX extends JavaPlugin {
         }else {
             System.out.println(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.RESET + "功能“AntiEnderManPickupBloc”未开启");
         }
-        int pluginId = 18219;
-        Metrics metrics = new Metrics(this, pluginId);
-        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
-        getServer().getScheduler().scheduleSyncDelayedTask(this,new TickPerSecond(),100L);
-        saveResource("keywords.yml",false);
-        getConfig().options().copyDefaults(true);
-        saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(new PlayerJoinEvent(),this);
-        getServer().getPluginManager().registerEvents(new PlayerBedEnterEvent(),this);
-        getServer().getPluginManager().registerEvents(new PlayerBedLeaveEvent(),this);
-        getServer().getPluginManager().registerEvents(new BadWords(),this);
-        getServer().getPluginManager().registerEvents(new KeepInventory(),this);
-        getServer().getPluginManager().registerEvents(new AntiCreeper(),this);
-        getServer().getPluginManager().registerEvents(new AntiCreeperBoom(),this);
-        getServer().getPluginManager().registerEvents(new AntiEnderMan(),this);
-        Objects.requireNonNull(getCommand("mpx")).setExecutor(new MengPluginXInfo());
-        Objects.requireNonNull(getCommand("gm")).setExecutor(new SetGameMode());
-        Objects.requireNonNull(getCommand("mpx_tps")).setExecutor(new GetTickPerSecond());
-        Objects.requireNonNull(getCommand("mpx_ping")).setExecutor(new GetPlayerPing());
-        Objects.requireNonNull(getCommand("online_player")).setExecutor(new GetOnlinePlayer());
-        Objects.requireNonNull(getCommand("mpx_seed")).setExecutor(new GetWorldSeed());
-        Objects.requireNonNull(getCommand("mpx_time")).setExecutor(new GetFullTime());
-        Objects.requireNonNull(getCommand("mpx_uuid")).setExecutor(new GetPlayerUUID());
-        Objects.requireNonNull(getCommand("mpx_ip")).setExecutor(new GetPlayerIP());
-        Objects.requireNonNull(getCommand("mpx_sa")).setExecutor(new SendAnnouncement());
-        Objects.requireNonNull(getCommand("mpx_ram")).setExecutor(new GetServerRam());
+        System.out.println(ChatColor.DARK_GREEN + "=========================");
     }
 
     @Override
     public void onDisable() {
-        System.out.println("插件成功关闭");
+        System.out.println(ChatColor.DARK_RED + "MengPluginX is disabled");
     }
 }
