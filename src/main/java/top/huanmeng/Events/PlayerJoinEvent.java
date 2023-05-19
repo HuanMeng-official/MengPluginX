253ef362531af5ea64d87843440be1d3cdcd7645
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-05-19 20:25:38 +0800

    添加加入服务器音效

--- src/main/java/top/huanmeng/Events/PlayerJoinEvent.java
+++ src/main/java/top/huanmeng/Events/PlayerJoinEvent.java
@@ -1,6 +1,9 @@
 package top.huanmeng.Events;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +16,11 @@ public class PlayerJoinEvent implements Listener {
     public void PlayerJoinEvent(org.bukkit.event.player.PlayerJoinEvent join){
         Player player = join.getPlayer();
         String name = join.getPlayer().getName();
-        player.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.WHITE + "欢迎" + name + "加入" + plugin.getConfig().getString("Server"));
+        join.setJoinMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.WHITE + "欢迎" + name + "加入" + plugin.getConfig().getString("Server"));
         player.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Tips"));
+        // 播放加入服务器音效
+        Location location = player.getLocation();
+        World world = location.getWorld();
+        world.playSound(location , Sound.ENTITY_PLAYER_LEVELUP,.5F,.5F);
     }
 }
