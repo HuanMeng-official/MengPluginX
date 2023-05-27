17579d422e0d1934d25529b5b3c2c0d0d5eed7f2
HuanMeng-official <3099044113@qq.com>
2023-05-27 16:54:27 +0800

    上传新版代码

---
+++ src/main/java/top/huanmeng/Events/PlayerBedLeaveEvent.java
@@ -0,0 +1,17 @@
+package top.huanmeng.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import top.huanmeng.MengPluginX;
+
+public class PlayerBedLeaveEvent implements Listener {
+    Plugin plugin = MengPluginX.getPlugin(MengPluginX.class);
+    @EventHandler
+    public void PlayerBedLeaveEvent(org.bukkit.event.player.PlayerBedLeaveEvent BedLeave){
+        Player getup = BedLeave.getPlayer();
+        getup.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Getup"));
+    }
+}
---
+++ src/main/java/top/huanmeng/Worlds/Functionality/AntiCreeper.java
@@ -0,0 +1,17 @@
+package top.huanmeng.Worlds.Functionality;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.Plugin;
+import top.huanmeng.MengPluginX;
+
+public class AntiCreeper implements Listener {
+    Plugin plugin = MengPluginX.getPlugin(MengPluginX.class);
+    @EventHandler
+    public void onCreeperBoom(EntityExplodeEvent event){
+        if (event.getEntityType().name().equals("CREEPER")) {
+            event.setCancelled(plugin.getConfig().getBoolean("AntiCreeper"));
+        }
+    }
+}
