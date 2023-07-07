0762874e51b3569cfcc639d10ff99d8f5537ce2b
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-07-07 14:22:01 +0800

    Add files via upload

---
+++ src/main/java/top/huanmeng/PlaceholderAPI/GetTickPerSecond.java
@@ -0,0 +1,24 @@
+package top.huanmeng.PlaceholderAPI;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class GetTickPerSecond implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String tps = "%server_tps%";
+            tps = PlaceholderAPI.setPlaceholders(player,tps);
+            sender.sendMessage(ChatColor.YELLOW + "[幻梦娘]" + ChatColor.RESET + "服务器当前的TPS为：" + ChatColor.GREEN + tps);
+        }else {
+            System.out.println(ChatColor.RED + "Error: Please let the player execute the command");
+        }
+        return false;
+    }
+}
