8e7467516223fff92706a7321239466ddbbb7246
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2023-08-31 03:10:57 +0800

    减少代码重复

--- src/main/java/top/huanmeng/Commands/CommandHelp.java
+++ src/main/java/top/huanmeng/Commands/CommandHelp.java
@@ -8,22 +8,20 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 
 public class CommandHelp implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage(ChatColor.GREEN + "------基础命令------");
-            player.sendMessage(ChatColor.YELLOW + "/mpx 插件信息");
-            player.sendMessage(ChatColor.GREEN + "------功能命令------");
-            player.sendMessage(ChatColor.YELLOW + "/search 搜索玩家背包物品");
-            player.sendMessage(ChatColor.YELLOW + "/mpx_ip 查看玩家IP（仅管理员）");
-            player.sendMessage(ChatColor.YELLOW + "/mpx_ping 查看自己与服务器的延迟");
-            player.sendMessage(ChatColor.YELLOW + "/mpx_tps 查看服务器当前TPS");
-            player.sendMessage(ChatColor.YELLOW + "/mpx_uuid 查看玩家UUID");
-            player.sendMessage(ChatColor.YELLOW + "/mpx_sa 发送公告");
-            player.sendMessage(ChatColor.YELLOW + "/online_player 获取当前服务器在线玩家数量");
+            for (String s : Arrays.asList(ChatColor.GREEN + "------基础命令------", ChatColor.YELLOW + "/mpx 插件信息")) {
+                player.sendMessage(s);
+            }
+            for (String s : Arrays.asList(ChatColor.GREEN + "------功能命令------", ChatColor.YELLOW + "/search 搜索玩家背包物品", ChatColor.YELLOW + "/mpx_ip 查看玩家IP（仅管理员）", ChatColor.YELLOW + "/mpx_ping 查看自己与服务器的延迟", ChatColor.YELLOW + "/mpx_tps 查看服务器当前TPS", ChatColor.YELLOW + "/mpx_uuid 查看玩家UUID", ChatColor.YELLOW + "/mpx_sa 发送公告", ChatColor.YELLOW + "/online_player 获取当前服务器在线玩家数量")) {
+                player.sendMessage(s);
+            }
             player.sendMessage(ChatColor.GREEN + "------世界命令------");
             player.sendMessage(ChatColor.YELLOW + "/mpx_seed 获取自己所在世界的种子");
             player.sendMessage(ChatColor.YELLOW + "/mpx_time 获取自己所在世界的绝对时间");
